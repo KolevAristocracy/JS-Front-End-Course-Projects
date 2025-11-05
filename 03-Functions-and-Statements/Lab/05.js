671ccee02d9bb4fe84cf6e732ef37255500022a8
Kalin Kolev <kalink300@gmail.com>
2025-11-05 16:22:21 +0200

    feat: Added lecture's way on task 05

--- 03-Functions-and-Statements/Lab/05.js
+++ 03-Functions-and-Statements/Lab/05.js
@@ -1,13 +1,64 @@
+// function simpleCalculator(num1, num2, operator) {
+//     let result;
+
+//     switch (operator) {
+//         case "multiply":
+//             result = multiply(num1, num2)
+//             break;
+
+//         case "divide":
+//             result = divider(num1, num2)
+//             break;
+
+//         case "add":
+//             result = add(num1, num2)
+//             break;
+
+//         case "subtract":
+//             result = subtract(num1, num2)
+//             break;
+
+//         default:
+//             break;
+//     }
+
+//     console.log(result);
+
+
+//     function multiply(a, b) {
+//         return a * b
+//     }
+
+//     function divider(a, b) {
+//         return a / b
+//     }
+
+//     function add(a, b) {
+//         return a + b
+//     }
+
+//     function subtract(a, b) {
+//         return a - b
+//     }
+// }
+
+
+// Lecture's way
 function simpleCalculator(num1, num2, operator) {
+    let multiply = (x, y) => x * y;
+    let divide = (x, y) => x / y;
+    let subtract = (x, y) => x - y;
+    let add = (x, y) => x + y;
+
     let result;
 
     switch (operator) {
-        case "multiply":
+            case "multiply":
             result = multiply(num1, num2)
             break;
 
         case "divide":
-            result = divider(num1, num2)
+            result = divide(num1, num2)
             break;
 
         case "add":
@@ -22,24 +73,7 @@ function simpleCalculator(num1, num2, operator) {
             break;
     }
 
-    console.log(result);
-    
-
-    function multiply(a, b) {
-        return a * b
-    }
-
-    function divider(a, b) {
-        return a / b
-    }
-
-    function add(a, b) {
-        return a + b
-    }
-
-    function subtract(a, b) {
-        return a - b
-    }
+    console.log(result)
 }
 
 simpleCalculator(5, 5, 'multiply')
