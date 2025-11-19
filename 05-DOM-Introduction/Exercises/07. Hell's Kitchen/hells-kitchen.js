f85452205d7d8facf87c50cec76686a6cbfcb260
Kalin Kolev <kalink300@gmail.com>
2025-11-19 19:01:05 +0200

    feat: Solved all tasks

--- 05-DOM-Introduction/Exercises/07. Hell's Kitchen/hells-kitchen.js
+++ 05-DOM-Introduction/Exercises/07. Hell's Kitchen/hells-kitchen.js
@@ -1,3 +1,41 @@
 function solve() {
-    //TODO
+    const input = document.querySelector('#inputs > textarea').value
+    const data = JSON.parse(input)
+    const restaurantInfo = {}
+
+    for (let element of data) {
+        let [restaurantName, employees] = element.split(' - ')
+        if (!Object.hasOwn(restaurantInfo, restaurantName)) restaurantInfo[restaurantName] = [];
+
+        for (let [employeeName, employeeSalary] of employees.split(', ').map((w) => w.split(" "))) {
+            restaurantInfo[restaurantName].push({ name: employeeName, salary: Number(employeeSalary) })
+        }
+    }
+
+    let bestRestaurant = "";
+    let bestAvgSalary = 0;
+
+    for (const restaurant in restaurantInfo) {
+        const staff = restaurantInfo[restaurant]
+        if (staff.length === 0) continue;
+
+        const avgSalary = staff.reduce((a, c) => a + c.salary, 0) / staff.length;
+        if (avgSalary > bestAvgSalary) {
+            bestAvgSalary = avgSalary;
+            bestRestaurant = restaurant;
+        }
+    }
+
+    const bestStaff = restaurantInfo[bestRestaurant]
+    const bestSalary = Math.max(...bestStaff.map((w) => w.salary));
+
+    const bestRestaurantOutput = document.querySelector("div#outputs > div#bestRestaurant > p");
+    bestRestaurantOutput.textContent = `Name: ${bestRestaurant} Average Salary: ${bestAvgSalary.toFixed(2)} Best Salary: ${bestSalary.toFixed(2)}`
+
+    const bestStaffOutput = document.querySelector("div#outputs > div#workers > p");
+    bestStaffOutput.textContent = bestStaff
+        .sort((a, b) => b.salary - a.salary)
+        .map((w) => `Name: ${w.name} With Salary: ${w.salary}`)
+        .join(" ");
+
 }
--- 05-DOM-Introduction/Exercises/09. Number Convertor/number-convertor.js
+++ 05-DOM-Introduction/Exercises/09. Number Convertor/number-convertor.js
@@ -1,3 +1,17 @@
 function solve() {
-    // TODO
+    const numberInput = Number(document.getElementById('input').value);
+    const toOption = document.querySelector('select#selectMenuTo').value;
+
+    let result = '';
+    if (toOption === 'hexadecimal') {
+        result = numberInput.toString(16).toUpperCase()
+    } else if (toOption === 'binary') {
+        result = numberInput.toString(2)
+    } else {
+        result = ''
+    }
+
+    const output = document.getElementById('result')
+
+    output.value = result
 }
