8076c5f527c47fe6c7f4dec7dce27a01ab4b33ca
Kalin Kolev <kalink300@gmail.com>
2025-12-04 15:46:23 +0200

    feat: Solved some of the exercise AJAX tasks

--- 09-HTTP-and-AJAX-Exercise-Resources/01.Blog/app.js
+++ 09-HTTP-and-AJAX-Exercise-Resources/01.Blog/app.js
@@ -1,5 +1,75 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+    // Post url http://localhost:3030/jsonstore/blog/posts
+    // Comments http://localhost:3030/jsonstore/blog/comments
+
+    const btnLoadPost = document.getElementById('btnLoadPosts');
+    const btnViewPost = document.getElementById('btnViewPost');
+    const selectEl = document.getElementById('posts');
+
+    btnLoadPost.addEventListener('click', populatePosts);
+    btnViewPost.addEventListener('click', viewPost);
+
+    async function loadPosts() {
+        const res = await fetch('http://localhost:3030/jsonstore/blog/posts');
+        const data = await res.json();
+        return data;
+    };
+
+    async function loadComments() {
+        const res = await fetch('http://localhost:3030/jsonstore/blog/comments');
+        const data = await res.json();
+        return data
+    }
+
+    async function populatePosts() {
+        const allPosts = await loadPosts();
+
+        selectEl.innerHTML = '';
+
+        for (const postId in allPosts) {
+            const post = allPosts[postId];
+            const option = document.createElement('option');
+
+            option.value = postId;
+            option.textContent = post.title;
+
+            selectEl.appendChild(option);
+        };
+    };
+
+    async function viewPost() {
+        const allPosts = await loadPosts();
+        const allComments = await loadComments();
+
+        const selectElId = selectEl.value;
+        const selectedPost = Object.values(allPosts)
+            .find(p => p.id === selectElId);
+
+        const allPostComments = Object.values(allComments)
+            .filter(c => c.postId === selectedPost.id);
+
+        h1El = document.getElementById('post-title')
+        pEl = document.getElementById('post-body');
+        ulEl = document.getElementById('post-comments');
+        
+        h1El.innerHTML = ''
+        pEl.innerHTML = ''
+        ulEl.innerHTML = ''
+        
+        h1El.textContent = selectedPost.title;
+        pEl.textContent = selectedPost.body;
+
+
+        
+
+        for (const comment of allPostComments) {
+            liEl = document.createElement('li');
+            liEl.id = comment.id;
+            liEl.textContent = comment.text;
+
+            ulEl.appendChild(liEl);
+        };
+    };
+};
 
 attachEvents();
--- 09-HTTP-and-AJAX-Exercise-Resources/02.Messenger/app.js
+++ 09-HTTP-and-AJAX-Exercise-Resources/02.Messenger/app.js
@@ -1,5 +1,41 @@
 function attachEvents() {
-    console.log('TODO...');
+    const textAreaEl = document.getElementById('messages');
+    const inputNameEl = document.querySelector('div#controls > input[name="author"]');
+    const inputMessageEl = document.querySelector('div#controls > input[name="content"]');
+
+    const submitBtn = document.getElementById('submit');
+    const refreshBtn = document.getElementById('refresh');
+
+    submitBtn.addEventListener('click', sendMsg);
+    refreshBtn.addEventListener('click', getAllMsg);
+
+    async function sendMsg() {
+        const authorName = inputNameEl.value;
+        const message = inputMessageEl.value;
+
+        await fetch('http://localhost:3030/jsonstore/messenger', {
+            method: "POST",
+            headers: {"Content-Type": "application/json"},
+            body: JSON.stringify({ "author": authorName, "content": message }),
+        });        
+
+        inputNameEl.value = ''
+        inputMessageEl.value = ''
+    };
+
+    async function getAllMsg() {
+        const res = await fetch('http://localhost:3030/jsonstore/messenger');
+        const data = await res.json();
+
+        textAreaEl.value = '';
+        for (const msg in data) {
+            const author = data[msg].author;
+            const content = data[msg].content;
+
+            textAreaEl.value += `${author}: ${content}\n`
+        };
+    };
+
 }
 
 attachEvents();
